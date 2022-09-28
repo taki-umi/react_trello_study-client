d022561026ad8319ba58d5e43d8034808bf0110c
TakumiTakikawa_react_container <takikawa.work@gmail.com>
2022-09-28 09:27:37 +0000

    タスク入力フォームの内容をstateで保持する対応

--- src/components/task/input/TaskAddInput.jsx
+++ src/components/task/input/TaskAddInput.jsx
@@ -1,14 +1,30 @@
 import React from "react";
 
-export const TaskAddInput = () => {
+export const TaskAddInput = ({
+  inputText,
+  setInputText,
+  setTaskList,
+  taskList,
+}) => {
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(e);
   };
+
+  const handleChange = (e) => {
+    setInputText(e.target.value);
+    console.log(inputText);
+  };
+
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="add a task" className="TaskAddInput" />
+        <input
+          type="text"
+          placeholder="add a task"
+          className="TaskAddInput"
+          onChange={handleChange}
+        />
       </form>
     </div>
   );
